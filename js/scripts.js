a628f38d43233f8504eae34e9afebb90082b9d29
Lucian <lucian.miller185@gmail.com>
2021-03-17 10:01:03 -0700

    It will make woof and meow list items appear on cat or dog side

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $("button#cat").click(function() {
-    $("ul").prepend("<li>Meow!</li?>");
+    $("ul#cat-said").prepend("<li>Meow!</li>");
+    $("ul#dog-said").prepend("<li>Woof!</li>");
   });
 
   $("button#dog").click(function() {
-    $("ul").prepend("<li>Woof!</li>")
+    $("ul#dog-said").prepend("<li>Woof!</li>");
+    $("ul#cat-said").prepend("<li>Meow!</li>");
   });
 });
